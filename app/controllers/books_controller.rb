e214ca6abf1c41a321a712f924b14d19c3563572
ysk178 <kk.ss.01tt.ht1985@gmail.com>
2020-11-23 16:40:00 +0900

    ユーザー管理機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,15 @@
 class BooksController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @books = Book.all
+  end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
